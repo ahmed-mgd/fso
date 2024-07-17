6d2dd34b43059e13012e16b3fae4b02c42def7af
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-17 13:20:22 -0400

    Fix ids

--- part3/helloworld/index.js
+++ part3/helloworld/index.js
@@ -4,17 +4,17 @@ app.use(express.json());
 
 let notes = [
   {
-    id: 1,
+    id: "1",
     content: "HTML is easy",
     important: true,
   },
   {
-    id: 2,
+    id: "2",
     content: "Browser can execute only JavaScript",
     important: false,
   },
   {
-    id: 3,
+    id: "3",
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
@@ -46,12 +46,26 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.post("/api/notes", (request, response) => {
+const generateId = () => {
   const maxId =
     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
 
-  const note = request.body;
-  note.id = String(maxId + 1);
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
 
   notes = notes.concat(note);
 

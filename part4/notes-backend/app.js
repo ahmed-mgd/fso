966c4ea33e105effad0edb8d633da88ddd129a70
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-31 17:04:10 -0400

    Testing Node applications

--- part4/notes-backend/app.js
+++ part4/notes-backend/app.js
@@ -1,97 +1,32 @@
-const express = require("express")
+const config = require('./utils/config')
+const express = require('express')
 const app = express()
-const cors = require("cors")
-const Note = require("./models/note")
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method)
-  console.log("Path:  ", request.path)
-  console.log("Body:  ", request.body)
-  console.log("---")
-  next()
-}
+mongoose.set('strictQuery', false)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" })
-}
+logger.info('connecting to', config.MONGODB_URI)
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" })
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
-app.use(requestLogger)
-app.use(express.static("build"))
+app.use(middleware.requestLogger)
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes)
-  })
-})
+app.use('/api/notes', notesRouter)
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote)
-    })
-    .catch((error) => next(error))
-})
-
-app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
-})
-
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
-})
-
-app.put("/api/notes/:id", (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedNote) => {
-      response.json(updatedNote)
-    })
-    .catch((error) => next(error))
-})
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
-
-module.exports = {
-  app,
-}
+module.exports = app
